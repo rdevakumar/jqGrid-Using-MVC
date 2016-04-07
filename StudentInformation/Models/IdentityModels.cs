40ef1ce321dd6229c3f59512d816b291fea7ca52
sourabh somani <sourabh_somani2010@hotmail.com>
2016-04-07 19:46:37 +0530

    Complete Project

---
+++ StudentInformation/Models/IdentityModels.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using StudentInformation.Entities;
+using System.Data.Entity;
+
+namespace StudentInformation.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+        public DbSet<StudentMaster> Students { get; set; }
+    }
+}
---
+++ StudentInformation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentInformation.Startup))]
+namespace StudentInformation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
